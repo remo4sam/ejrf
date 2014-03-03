2fe2d1ab55eff3bb8f68c9f7666eda55cc94da25
Emily Namugaanyi <emilynam@thoughtworks.com>
2014-03-03 15:53:31 +0300

    now edit section comes in a modal

--- questionnaire/static/js/preview_questionnaire.js
+++ questionnaire/static/js/preview_questionnaire.js
@@ -25,6 +25,17 @@ $(function(){
         });
     });
 
+    $('.edit_section').on('show.bs.modal', function(){
+        var section_id = $(this).attr('object-id'),
+            assign_to_subsection_url = "/section/" + section_id +"/edit/";
+        $.get(assign_to_subsection_url, function( data ) {
+            var $holder = $('<div></div>').append(String(data));
+            var content =  $holder.find("#form-content").html()
+            $( "#edit_section_"+section_id+"_ajax_content").html(content);
+            $('textarea').autosize();
+        });
+    });
+
 
 });
 
