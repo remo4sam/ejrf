0c912d86620839f01db114ec8968cc8a8edb5815
Tom Oketch <tc.oketch@gmail.com>
2014-03-07 12:47:03 +0300

    Add functional test for Update Subsection in Core Questionnaire

--- questionnaire/features/subsection.feature
+++ questionnaire/features/subsection.feature
@@ -8,3 +8,14 @@ Feature: Subsection feature
         When i fill in the subsection data
         And I save the subsection
         Then I should see the subsection I just created
+
+    Scenario: Update Subsection in Core Questionnaire
+        Given I am logged in as a global admin
+        And I have a questionnaire with sections and subsections
+        And I visit that questionnaires section page
+        And I choose to update a subsection
+        Then I should see an edit subsection modal
+        When I update the subsection details
+        And I save the changes to the subsection
+        Then I should see a message that the subsection was updated
+        And I should see the changes I made to the subsection in the questionnaire
--- questionnaire/features/subsection_steps.py
+++ questionnaire/features/subsection_steps.py
@@ -30,3 +30,31 @@ def then_i_should_see_the_subsection_i_just_created(step):
     world.page = QuestionnairePage(world.browser, world.section_1)
     world.page.is_text_present('Subsection successfully created.')
     world.page.is_text_present('Some title')
+
+@step(u'And I choose to update a subsection')
+def and_i_choose_to_update_a_subsection(step):
+    world.page.click_by_id('edit-subsection-%s' % world.sub_section.id)
+
+@step(u'Then I should see an edit subsection modal')
+def then_i_should_see_an_edit_subsection_modal(step):
+    world.page = CreateSubSectionPage(world.browser, world.questionnaire, world.section_1)
+    world.page.is_text_present("Edit SubSection", "Title", "Description")
+
+@step(u'When I update the subsection details')
+def when_i_update_the_subsection_details(step):
+    data = {'title': 'New SubSection Name',
+            'description': 'New SubSection Title'}
+    world.page.fill_this_form('#edit_subsection_%s_modal_form' % world.sub_section.id, data)
+
+@step(u'And I save the changes to the subsection')
+def and_i_save_the_changes_to_the_subsection(step):
+    world.page.click_by_id('submit_edit_subsection_%s' % world.sub_section.id)
+
+@step(u'Then I should see a message that the subsection was updated')
+def then_i_should_see_a_message_that_the_subsection_was_updated(step):
+    world.page.is_text_present('SubSection updated successfully')
+
+@step(u'And I should see the changes I made to the subsection in the questionnaire')
+def and_i_should_see_the_changes_i_made_to_the_subsection_in_the_questionnaire(step):
+    world.page.is_text_present('New SubSection Name')
+    world.page.is_text_present('New SubSection Title')
