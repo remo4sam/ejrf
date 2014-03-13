8539bf5fbe109a0b45eb52b71a51ebe72cb7522d
Neil Coutinho <ncoutin@thoughtworks.com>
2014-03-13 11:57:27 +0300

    Fix finalize for safari, firefox.

--- questionnaire/static/js/finalize_questionnaire.js
+++ questionnaire/static/js/finalize_questionnaire.js
@@ -1,23 +1,22 @@
 function makePostRequest(url, data) {
-    console.log(url);
-    console.log(data);
     var jForm = $('<form></form>');
     jForm.attr('action', url);
     jForm.attr('method', 'post');
     for (name in data) {
-        var jInput = $("<input>");
-        jInput.attr('name', name);
-        jInput.attr('value', data[name]);
+        var jInput = $("<input/>");
+        jInput.attr({'name' : name, 'value': data[name], 'type': 'hidden'});
         jForm.append(jInput);
     }
-    //console.log(jForm);
-    jForm.submit();
+    var button = $("<input type='submit' style='display: none'/>");
+    jForm.append(button);
+    $("body").append(jForm);
+    button.trigger('click');
 }
 
 $(function(){
     $("a[post=true]").each(function () {
-        console.log('here');
-        $(this).on('click', function () {
+        $(this).on('click', function (e) {
+            e.preventDefault();
             makePostRequest(
                 $(this).attr('phref'),
                 JSON.parse($(this).attr('pdata'))
