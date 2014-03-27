5e5940b3a656fc72892cc65e0bbb1af001c590cd
Charles Tumwebaze <ctumwebaze@gmail.com>
2014-03-27 15:11:22 +0300

    [Charles/Emily] fix to show only global finalized and published questionnaires when duplicating.

--- questionnaire/forms/questionnaires.py
+++ questionnaire/forms/questionnaires.py
@@ -5,7 +5,7 @@ from questionnaire.utils.form_utils import _set_year_choices
 
 
 class QuestionnaireFilterForm(forms.Form):
-    questionnaire = forms.ModelChoiceField(queryset=Questionnaire.objects.filter(status__in=[Questionnaire.FINALIZED, Questionnaire.PUBLISHED]),
+    questionnaire = forms.ModelChoiceField(queryset=Questionnaire.objects.filter(status__in=[Questionnaire.FINALIZED, Questionnaire.PUBLISHED], region=None),
                                            empty_label="Select Questionnaire",
                                            widget=forms.Select(attrs={"class": 'form-control'}), required=True)
     year = forms.ChoiceField(widget=forms.Select(attrs={"class": 'form-control'}), required=True, choices=[])
